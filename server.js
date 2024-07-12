ae73878b262ad85481315d3501664c1c7bca454b
lijinlawrence <kittulawrence@gmail.com>
2024-07-12 16:43:00 +0530

    cor updated

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require("express");
 const app = express();
 require("dotenv").config();
 const connectDB = require("./config/db");
-const port = parseInt(process.env.PORT);
 const userRouter = require("./routes/userRoute");
 const videoRouter = require("./routes/videoRoutes");
 const cors =  require('cors')
@@ -13,15 +12,22 @@ connectDB();
 app.use(bodyParser.json({limit: '50mb'})); 
 app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 console.log(process.env.PORT);
+// CORS Configuration
+const corsOptions = {
+  origin: 'http://localhost:3000', // Replace with your frontend URL
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
+  allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
+};
 
 app.use(express.json());
-app.use(cors())
+app.use(cors(corsOptions))
 app.use("/user", userRouter);
 app.use("/video", videoRouter);
 app.get("/", (req, res) => {
   res.send(`Server is Working`);
 });
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
-  console.log(`Server is up and Run with http://localhost:${process.env.PORT}`);
+  console.log(`Server is up and Run with http://localhost:${port}`);
 });
